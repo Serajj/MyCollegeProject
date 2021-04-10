31cd45508b3f46de286a90a40f9c787494ab6419
Serajj <rajalam2372@gmail.com>
2021-04-10 15:25:58 +0530

    server port changed

--- app.js
+++ app.js
@@ -26,6 +26,6 @@ app.use('/api/users', userRouter);
 
 
 
-app.listen(process.env.SERVER_PORT, () => {
-    console.log("Server is running on port " + process.env.SERVER_PORT)
+app.listen(3000, () => {
+    console.log("Server is running on port 3000")
 })
